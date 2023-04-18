207ac16bbe4b93509539fac646230a737a99dcd0
contato12 <contato12araujo@gmail.com>
2023-04-18 15:12:13 -0300

    Primeiro Projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
-    path: 'folder/:id',
-    loadComponent: () =>
-      import('./folder/folder.page').then((m) => m.FolderPage),
+    path: 'home',
+    loadComponent: () => import('./pages/components/home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: 'button',
+    loadComponent: () => import('./pages/components/button/button.page').then(m => m.ButtonPage)
+  },
+  {
+    path: 'card',
+    loadComponent: () => import('./pages/components/card/card.page').then( m => m.CardPage)
   },
 ];
